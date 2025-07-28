c5f788fed868c0e3752c0092eb1fce4712bb7880
JackBreeton <jackbreeton@gmail.com>
2025-07-28 09:34:47 +0100

    Project 1 start

---
+++ Project 1/ConsoleCalculator/Calculator.cs
@@ -0,0 +1,24 @@
+namespace ConsoleCalculator
+{
+
+    public static class Calculator
+    {
+        static public double Add (double a,  double b)
+        {
+            return a + b;
+        }
+        static public double Subtract (double a,  double b)
+        {
+            return a - b;
+        }
+        static public double Multiply (double a,  double b)
+        {
+            return a * b;
+        }
+        static public double Divide (double a,  double b)
+        {
+            return a / b;
+        }
+    }
+
+}
---
+++ Project 1/ConsoleCalculator/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+
+            // Loop the app so that it runs until closed
+            while(true)
+            {   
+                // ensure character is correct within call
+                Console.WriteLine("Input your first Number: ");
+                string numberOne = Console.ReadLine();
+
+                Console.WriteLine("Input your Operator: ");
+                string operation = Console.ReadLine();
+                while (operation != )
+
+                Console.WriteLine("Input your Second Number: ");
+                string numberTwo = Console.ReadLine(); 
+
+
+                // Updated double call to add character Parse
+                double result;
+                if(!double.TryParse(numberOne, out double numOne) ||
+                   !double.TryParse(numberTwo, out double numTwo))
+                {
+                    Console.WriteLine("Character is undefined please enter correct character!");
+                    return;
+                }
+                // double numOne = Convert.ToDouble(numberOne);
+                // double numTwo = Convert.ToDouble(numberTwo);
+
+
+                switch (operation) 
+                {
+                    case "+":
+                        // Method call
+                        result = Calculator.Add(numOne, numTwo);
+                        Console.WriteLine(result);
+                        break;
+                    case "-":
+                        // Method call
+                        result = Calculator.Subtract(numOne, numTwo);
+                        Console.WriteLine(result);
+                        break;
+                    case "*":
+                        // Method call
+                        result = Calculator.Multiply(numOne, numTwo);
+                        Console.WriteLine(result);
+                        break;
+                    case "/":
+                        // Method call
+                        if (numOne == 0 || numTwo == 0)
+                        {
+                            Console.WriteLine("Undefined");
+                        }
+                        else 
+                        {
+                            result = Calculator.Divide(numOne, numTwo);
+                            Console.WriteLine(result);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid operator, please use +  -  * or /");
+                        break;
+                }
+
+            Console.Write("Do you want to calculate again? (y/n): ");
+            if (Console.ReadLine().ToLower() != "y")
+                break;
+
+            }
+        }
+    }
+}
+
